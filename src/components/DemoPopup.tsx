faa126969668c32ac0f26db37e9d3d859471f4c9
ChiragKV-Juspay <chirag.kv@juspay.in>
2026-05-17 14:03:41 +0530

    added subscription events for testing

--- src/components/DemoPopup.tsx
+++ src/components/DemoPopup.tsx
@@ -128,6 +128,7 @@ export default function DemoPopup({ onClose }: DemoPopupProps) {
             loadingSaved={true}
             canSubmit={false}
             isCvcComplete={false}
+            formStatus={null}
             amount={amount}
             updateAmount={null}
             widgets={null}
